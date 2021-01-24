56a6681651c3148bafe730f6b7a7fe7733879ab4
吴乐川 <wulechuan@live.com>
2021-01-24 23:54:39 +0800

    更新了辅助性脚本工具。

--- .ps-git-push-to-all-origins.ps1
+++ .ps-git-push-to-all-origins.ps1
@@ -1,8 +1,7 @@
 # 本文件之文件名冠以英语句点“.”，是为了在 Shell 或 Git Bash 环境下，令本文件不可见。
-# 而在文件名起始的英语句点之后继续冠以“ps-”（代表“Power Shell”）短语，
-# 则是为了避免本文件与 Git 系统的“.git”文件夹的起始部分相同。一旦令文件名
-# 与“.git”文件夹的起始部分有区别，则日常在 Windows 环境借助 tab 键自动补
-# 齐本文件全称就更便捷了。
+# 而在文件名起始的英语句点之后继续冠以“ps-”（代表“Power Shell”）短语，则是为了避免
+# 本文件与 Git 系统的“.git”文件夹的起始部分相同。一旦令文件名与“.git”文件夹的起始部
+# 分有区别，则日常在 Windows 环境借助 tab 键可自动补齐本文件全称就更便捷了。
 
 . .\PowerShell-Tools\common\Push-GitRepositoryToSingleOrigin.ps1
 
